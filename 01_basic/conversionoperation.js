27fd87e31bf50c34db5b91cac49bf411585aea07
kanavcode19 <kanavr577@gmail.com>
2026-02-14 09:23:06 +0000

    conversion of database

---
+++ 01_basic/conversionoperation.js
@@ -0,0 +1,19 @@
+let score="33abc"
+
+// console.log(score);
+// console.log(typeof score);
+
+let valueInNum = Number(score)
+// console.log(typeof valueInNum );
+// console.log(valueInNum);
+
+let islogedin = "kanav"
+let booleanvalue=Boolean(islogedin)
+
+// console.log(booleanvalue);
+
+let string=33
+let numstring = String(string)
+
+console.log(typeof numstring);
+console.log(numstring);
