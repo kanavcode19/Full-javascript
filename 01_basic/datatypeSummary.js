27464d4e203b559f3ec9ac4dc53f2c902d7330f8
kanavcode19 <kanavr577@gmail.com>
2026-02-14 11:25:51 +0000

    stack heap summary

---
+++ 01_basic/datatypeSummary.js
@@ -0,0 +1,9 @@
+let userone = {
+    email: "kitti12@gmail.com",
+    upi:"upird2",
+}
+
+let usertwo = userone
+usertwo.email = "kanuu@google.com"
+console.log(userone.email)
+console.log(usertwo.email)
