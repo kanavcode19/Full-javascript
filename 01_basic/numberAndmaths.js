3c840c6715ea9ce85b4364e634ac0134285555a7
kanavcode19 <kanavr577@gmail.com>
2026-02-16 13:06:59 +0000

    string

---
+++ 01_basic/numberAndmaths.js
@@ -0,0 +1,20 @@
+// const balance = new Number(200);
+// console.log(balance);
+
+// const othernumber = 123.23
+// console.log(othernumber.toPrecision(4))
+
+// const hundred = 100000;
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+//**********************MATH*************************
+
+// console.log(Math);
+// console.log(Math.round(4.4)) //4
+// console.log(Math.ceil(4.1)) //5
+// console.log(Math.floor(4.9))  //4
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
---
+++ 01_basic/string.js
@@ -0,0 +1,20 @@
+const name = "Kanav"
+const sui = 900
+// console.log(name);
+
+console.log(`Hello my name is ${name} and today i am very upset ${sui}`);
+
+//     const gameName = new String('kanavrajput')
+// console.log(gameName);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2))
+
+// const newSubstring = gameName.substring(0,4);
+// console.log(newSubstring)
+// const anotherstring = gameName.slice(-8,5)
+// console.log(anotherstring)
+
+const url="http//:kanav.com/kanav%20rajput"
+console.log(url.replace('%20','-'))
+
